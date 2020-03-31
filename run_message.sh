8869973b800c2e18d82880d5f344dabff829a847
geeker <1632298575@qq.com>
2020-04-01 01:11:47 +0800

    修改单一的判断为判断模式和循环执行模式

--- run_message.sh
+++ run_message.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #实现按任意键继续执行
 function pause(){
   get_char()
@@ -23,12 +24,16 @@ function open_crack_website(){
     gnome-open https://zhile.io/2018/08/25/jetbrains-license-server-crack.html #文件下载
     gnome-open https://segmentfault.com/a/1190000021736634    #使用方法
 }
+
 #出错，查看teminal信息
-function if_error(){
+if_error(){
 #运行成功返回0，失败返回1
     result=$?
     if [ $result  -eq 0 ];then
-   	echo 'Congratulations,某部分执行通过'
+	gxmessage -center \
+          -buttons "Amd Yes":1,"Trump No":2 \
+          -geometry 400x200 \
+          -title "运行成功提示" 'Congratulations,某部分执行通过'
     else
 	gxmessage -center \
           -buttons "Amd Yes":1,"Trump No":2 \
@@ -37,12 +42,45 @@ function if_error(){
 	exit 
     fi
 }
+param_processing(){
+    if [ $1 -eq 0 ];then
+	    echo -e "\033[32m------判断模式------ \033[0m"　
+	    echo '123456'
+            $input_param2
+            if_error      #函数嵌套调用需要和判断语句齐平书写，否则报错
+    	elif [ $1 -eq 1 ];then
+	    echo -e "\033[32m--------------循环执行模式-------------- \033[0m"
+	    echo -e "\033[32m----------------第1次执行----------------\033[0m"
+    	    $input_param3 #执行命令行输入的真正语句　
+	    while [ $? != 0 -a $input_param2 -gt $counter ]
+	    do
+		((counter++))
+		echo -e "\033[32m----------------第$counter次执行----------------\033[0m"
+		$input_param3
+	    done
+    fi
+}
+function message_box(){
+    gxmessage -center \
+          -buttons "Amd Yes":1,"Trump No":2 \
+          -geometry 400x200 \
+          -title "Message Notes" " ${@:1}"
+}
+
 ################################正文开始#############################
-#update   #自定义函数
-echo -e '\033[37m脚本即将开始执行'
-${@:1}
-if_error
-echo '脚本执行结束'
+#使用说明
+#只检查代码是否执行成功模式
+#    文件运行时加两个参数，第一是０，第二个为需要执行的代码
+#循环执行代码模式
+#    文件运行时三个参数，第一个是１，第二个数字为执行的次数，第三个为执行的代码
+echo -e "\033[32m脚本即将开始执行\033[0m"
+counter=1 #记录执行语句次数
+input_param1=$1
+input_param2=$2
+input_param3=${@:3}
+param_processing $input_param1
+echo -e "\033[32m共执行$counter次\033[0m"
+echo -e "\033[32m脚本执行结束\033[0m"
 
 
 
